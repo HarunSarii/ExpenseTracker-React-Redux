5b03add26d691cd7d9950eb51dccbd427b6eb06a
HarunSarii <harunsari14@gmail.com>
2021-09-15 20:09:29 +0300

    Add Header and Balance

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      expense
+      <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
   );
 }
