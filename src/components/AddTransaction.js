397ce537eeaa9ff285686a32170a785c4b893cd9
HarunSarii <harunsari14@gmail.com>
2021-09-17 04:30:21 +0300

    Add and Delete Transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { v4 as uuid } from 'uuid'
+
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0)
+
+    const { addTransaction } = useContext(GlobalContext)
+    console.log('addT:', addTransaction)
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id: uuid(),
+            text,
+            amount: +amount //to convert string to a number
+        }
+        addTransaction(newTransaction)
+        console.log('newT', newTransaction, 'id:', newTransaction.id)
+    }
     return (
         <>
             <h3>Add new transaction</h3>
-            <form  >
+            <form onSubmit={onSubmit} >
                 <div className='form-control' >
                     <label htmlFor="text" >Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...' />
